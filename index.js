4af552a217eb6ebda0b0397b620c2c52e5a60f14
YusufGuducu <yusufguducu23@hotmail.com>
2024-10-01 12:38:22 +0300

    proje tamamlandı

---
+++ index.js
@@ -0,0 +1,36 @@
+let counter = 0
+let counterDOM = document.querySelector('#counter')
+let increaseDOM = document.querySelector('#increase')
+let decreaseDOM = document.querySelector('#decrease')
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click" ,clickEvent)
+decreaseDOM.addEventListener("click" ,clickEvent)
+
+// İlk Yöntem
+
+// function clickEvent() {
+//     console.log(this.id)
+//     if (this.id == "increase") {
+//         counterDOM.innerHTML = counter += 1
+//     } else {
+//         counterDOM.innerHTML = counter -= 1
+//     }
+// }
+
+// İkinci Yöntem
+
+// function clickEvent() {
+//     console.log(this.id)
+//     this.id == "increase" ? counterDOM.innerHTML = counter += 1 
+//     :counterDOM.innerHTML = counter -= 1
+//     }
+
+// Üçüncü Yöntem 
+
+function clickEvent() {
+    console.log(this.id)
+    this.id == "increase" ? counter += 1 : counter -= 1
+    counterDOM.innerHTML = counter 
+    }
